751070f2e25553b2875bb7e3e340633faba58cc8
Daniel <daniel.frengl@gmail.com>
2024-09-25 08:42:49 +0200

    feat: init

---
+++ index/index.php
@@ -0,0 +1,23 @@
+<?php
+
+
+$db_server = "localhost";
+$db_username = "root";
+$db_password = "";
+$db_name = "db2";
+
+$conn = mysqli_connect('localhost', 'root', '', 'db2');
+
+
+if ($conn) {
+    echo"<br> <br>Databáze připojena.";
+
+}
+
+else {
+    echo"Připojení selhalo.";
+}
+
+
+$conn->close();
+?>
