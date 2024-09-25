bee798bdb39b7fed47b8fd9d238e76154e6f9091
Daniel <daniel.frengl@gmail.com>
2024-09-25 09:06:15 +0200

    php

---
+++ index/funkce.php
@@ -0,0 +1,28 @@
+<?php
+
+
+$db_server = "localhost";
+$db_username = "root";
+$db_password = "";
+$db_name = "db2";
+
+$conn = mysqli_connect('localhost', 'root', '', 'db2');
+
+
+
+
+if ($conn) {
+    echo"<br> <br>Databáze připojena.";
+
+}
+
+else {
+    echo"Připojení selhalo.";
+}
+
+
+$conn->close();
+
+echo "ses kokot";
+echo "ses kokot";
+?>
